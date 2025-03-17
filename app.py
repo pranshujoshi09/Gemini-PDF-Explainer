17d4000017823ba53249edf5f2226f29fa1a7101
Pranshu Joshi <pranshujoshi927@gmail.com>
2025-03-17 16:14:55 +0530

    Update app.py

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ st.subheader("Prompt")
 col1, col2 = st.columns([3.5,1.5])
 
 with col1:
-    prompt = st.text_area("  ", height=120,
+    prompt = st.text_area("  ", height=130,
                      placeholder="Ask anything..")
 with col2:
     pdf = st.file_uploader("  ", type=["pdf"])
@@ -89,3 +89,68 @@ if st.button("Analyze PDF"):
     if pdf is not None:
             generate()
 
+col3, col4, col5, col6, col7 = st.columns([1, 1.2, 1, 1, 1])
+selected_subject = None  
+
+with col3:
+    if st.button("Physics"):
+        selected_subject = "Physics"
+
+with col4:
+    if st.button("Chemistry"):
+        selected_subject = "Chemistry"
+
+with col5:
+    if st.button("Biology"):
+        selected_subject = "Biology"
+
+with col6:
+    if st.button("Maths"):
+        selected_subject = "Maths"
+
+with col7:
+    if st.button("Gujarati"):
+        selected_subject = "Gujarati"
+
+if selected_subject:
+    st.subheader(f"{selected_subject} Prompts")
+
+    prompts = {
+        "Physics": """
+        - આ પ્રકરણને સૌથી સરળ રીતે સમજાવો
+        - આ પ્રકરણ મને વાસ્તવિક જીવનના ઉદાહરણો સાથે સમજાવો. 
+        - આ પ્રકરણમાં આવતા વિવિધ મુદ્દાઓની વ્યાખ્યા આપો
+        - મને આ વિષયને લગતા વિવિધ પરીક્ષાલક્ષી દાખલા આપો.
+        - વાસ્તવિક જીવનમાં આ પ્રકરણનું શું મહત્વ છે?   
+        """,
+        "Chemistry": """
+        - આ પ્રકરણમાં આવતા વિવિધ રાસાયણિક સમીકરણોની યાદી બનાવો.
+        - આ રાસાયણિક પ્રક્રિયાનું સમીકરણ અને પરિણામ શું છે? આ રાસાયણિક પ્રક્રિયાનો ઉપયોગ પણ જણાવો.
+        - આ પ્રકરણમાં કઈ મૂળભૂત બાબતો મને ઝડપથી શીખવામાં મદદ કરશે?
+        - આ પ્રકરણ સમજવા માટે મને મદદરૂપ "notes" આપો.
+        - આ પ્રકરણને લાંબા સમય સુધી યાદ રાખવાની શ્રેષ્ઠ રીત કઈ છે? 
+        """,
+        "Biology": """
+        - આ પ્રકરણમાંથી મને મહત્વપૂર્ણ મુદ્દાઓ આપો.
+        - આ પ્રકરણ સમજવા માટે મને મદદરૂપ notes  આપો.  
+        - શું તમે આ મુદ્દાને એવી રીતે સમજાવી શકો છો કે તેઓ એક દેશ છે?
+        - આ પ્રકરણના બધા જ પારિભાષિક શબ્દો સરળ શબ્દોમાં સમજાવો. 
+        - આ પ્રકરણને અસરકારક રીતે સમજવા માટે મને તેનો એક Roadmap આપો. 
+        """,
+        "Maths": """
+        - આ પ્રકરણમાં આવતા સૂત્રની યાદી આપો.
+        - આ દાખલા ને સૌથી સરળ અને અસરકારક રીતે ઉકેલો.  
+        - આ પ્રકરણને લગતા વિવિધ દાખલા પ્રેક્ટિસ માટે આપો. 
+        - આ પ્રકરણ માં આવતી થિયરી સમજાવો.
+        - આ પ્રકરણમાંથી પરીક્ષાલક્ષી MCQ પ્રશ્ન આપો.
+        """,
+        "Gujarati": """
+        - આ પ્રકરણની વાર્તાનો સારાંશ લગભગ 50 શબ્દોમાં આપો. 
+        - આ પ્રકરણનો કૃતિ અને કર્તા પરિચય આપો,
+        - આ પ્રકરણના સમાનાર્થી, વિરોધી શબ્દો, રૂઢિપ્રયોગો,કહેવત,શબ્દસમૂહ,વગેરે આપો.  
+        - આ પ્રકરણમાં આપેલા પ્રશ્નનો વિસ્તારથી લગભગ ૧૦૦ શબ્દો માં જવાબ આપો. 
+        - આ પ્રકરણમાંથી પરીક્ષામાં કયા પ્રશ્નો હોઈ શકે?  
+        """
+    }
+
+    st.markdown(prompts[selected_subject]) 
